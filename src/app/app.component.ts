88fdb3f966dc70ed1c0fee06d443dd756ba6a393
Chakri Chikkaala <chakridol143@gmail.com>
2025-03-14 20:17:56 +0530

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,52 @@ import { RouterOutlet } from '@angular/router';
 })
 export class AppComponent {
   title = 'Rock-Paper-Scissor';
+  result:string =""
+  abc:string="";
+  computer:string="Let's Play"
+  CpuScore:number=0;
+  UserScore:number=0;
+  num:number=0
+  Restart(){
+    window.location.reload();
+  }
+
+  myfun(user:any){
+    this.abc="Computer has selected - "
+  let s:number = Math.random()
+  if(s >=0.0 && s<=0.33){
+    this.computer='Rock'
+    }
+  else if(s >=0.68 && s<=1.0){
+      this.computer='Scissor'
+     }
+  else if(s >=0.34 && s<=0.67){
+        this.computer='Paper'
+    }
+    if(user==this.computer){
+      this.result="Match drawn."
+    }
+  else if(user=="Rock" && this.computer=="Paper"){
+    this.result="Computer has Won"
+    this.CpuScore +=1
+    
+  }
+  else if(user=="Rock" && this.computer=="Scissor"){
+    this.result="You have Won";
+    this.UserScore +=1
+  }else if(user=="Paper" && this.computer=="Rock"){
+    this.result="You have Won"
+    this.UserScore +=1
+  }else if(user=="Paper" && this.computer=="Scissor"){
+    this.result="Computer has Won"
+    this.CpuScore +=1
+  }else if(user=="Scissor" && this.computer=="Paper"){
+    this.result="You have Won"
+    this.UserScore +=1
+  }else if(user=="Scissor" && this.computer=="Rock"){
+    this.result="Computer has Won"
+    this.CpuScore +=1
+  }
+
+  }
 }
